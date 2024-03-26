36ae7d4667493e22174f23f84284d0695c68108a
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-03-25 21:34:12 -0300

    finalizacao

--- index.php
+++ index.php
@@ -10,16 +10,28 @@
     <div class="container">
         <h1 class="text-center">CARD GAME</h1>
         <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-12">
+            <?php
+            $arquivo = fopen("dados.csv", "r");
+            while(!feof($arquivo)):
+                $linha = fgets($arquivo, 1000);
+                if($linha != ""):
+                $dados = explode(";" ,$linha);
+                ?>
+                <div class="col-lg-4 col-md-4 col-sm-12">
                 <div class="card" style="width: 18rem;">
-                    <img src="foto01.jpg" class="card-img-top" alt="mae-arvore">
+                    <img src="<?php echo $dados[2] ?>" class="card-img-top" alt="<?php echo $dados[0] ?>">
                     <div class="card-body">
-                        <h5 class="card-title">Mãe Árvore</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title"> <?php echo $dados[0] ?> </h5>
+                        <p class="card-text"> <?php echo $dados[1] ?> </p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-            </div>
+                    </div>
+                </div>
+                </div>
+                <?php
+                endif;
+            endwhile;
+            ?>
+            
         </div>
     </div>
 
